d1805baf990d19cc046ee03629c2889a91f873c8
David Dashifen Kees <dashifen@dashifen.com>
2021-01-03 08:02:56 -0500

    mobile menu

--- index.php
+++ index.php
@@ -7,23 +7,35 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class() ?>>
-<div id="vue-root">
+<div class="container">
   <a href="#content" class="screen-reader-text">Skip to the Content</a>
   <header class="banner" aria-labelledby="page-title">
-    <h1 id="page-title">The Firefly House</h1>
-    <ul class="main-menu" aria-label="main menu">
-      <li><a href="/">Home</a></li>
-      <li><a href="/">About</a></li>
-      <li><a href="/">Events</a></li>
-      <li><a href="/">Tarot</a></li>
-      <li><a href="/">Policies</a></li>
-      <li><a href="/">Contact</a></li>
-    </ul>
+    <a href="/">
+      <h1 id="page-title">The Firefly House</h1>
+    </a>
+
+    <div class="toggler">
+      <a id="toggler" class="screen-reader-text">Click or touch to toggle main menu availability</a>
+      <div class="mid-line"></div>
+    </div>
+
+    <nav class="main-menu" aria-label="main menu">
+      <ul>
+        <li class="menu-item"><a class="menu-item-link" href="/">Home</a></li>
+        <li class="menu-item"><a class="menu-item-link" href="/">About</a></li>
+        <li class="menu-item"><a class="menu-item-link" href="/">Events</a></li>
+        <li class="menu-item"><a class="menu-item-link" href="/">Tarot</a></li>
+        <li class="menu-item"><a class="menu-item-link" href="/">Policies</a></li>
+        <li class="menu-item"><a class="menu-item-link" href="/">Contact</a></li>
+      </ul>
+    </nav>
   </header>
-      
-  <main id="content" class="main"></main>
-  <section class="main__sidebar"></section>
-  <footer class="footer"></footer>
+
+  <div class="body">
+    <main id="content" class="main"></main>
+    <section class="main__sidebar"></section>
+    <footer class="footer"></footer>
+  </div>
 </div>
 <?php wp_footer() ?>
 </body>
--- src/Theme.php
+++ src/Theme.php
@@ -33,7 +33,8 @@ class Theme extends AbstractThemeHandler
    */
   protected function addAssets(): void
   {
-    $this->enqueue('assets/firefly.js');
+    $this->enqueue('//fonts.googleapis.com/css2?family=Kaushan+Script&family=Lato&display=swap');
     $this->enqueue('assets/firefly.css');
-  }
+    $this->enqueue('assets/firefly.js');
+   }
 }
